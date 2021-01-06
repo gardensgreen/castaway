c6c3fa53b30adaad1117619b8b08b6b813ece32e
Daniel TIllero <danieltillero67@gmail.com>
2021-01-05 20:32:16 -0500

    add react proxy

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,35 @@
 import os
 from flask import Flask, render_template, request, session
+from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+
 from app.models import db, User
 from app.config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 app.config.from_object(Config)
 db.init_app(app)
 
+# Application Security
+CORS(app)
 
-@app.route("/")
-def index():
-    return ('Index Works')
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie('csrf_token',
+                        generate_csrf(),
+                        secure=True if os.environ.get('FLASK_ENV') else False,
+                        samesite='Strict' if os.environ.get(
+                            'FLASK_ENV') else None,
+                        httponly=True)
+    return response
+
+
+@app.route("/", defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file("favicon.ico")
+    return app.send_static_file('index.html')
