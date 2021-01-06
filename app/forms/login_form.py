d977cb4dbe989a67a4a73028a372f601a5d778b8
Daniel TIllero <danieltillero67@gmail.com>
2021-01-06 10:50:54 -0500

    Fix password_matches function

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -15,6 +15,8 @@ def password_matches(form, field):
     password = field.data
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError("No such user exists")
     if not user.check_password(password):
         raise ValidationError("Password was incorrect")
 
