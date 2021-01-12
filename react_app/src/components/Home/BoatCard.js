c65598221bb4a40e4162a139aca9564b466029eb
Daniel TIllero <danieltillero67@gmail.com>
2021-01-12 12:53:45 -0500

    boat card

--- react_app/src/components/Home/BoatCard.js
+++ react_app/src/components/Home/BoatCard.js
@@ -4,17 +4,19 @@ import styled from "styled-components";
 const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
-    width: 400px;
+    width: 300px;
     margin-right: 30px;
     margin-bottom: 30px;
-    border-radius: 3px;
-    border: 1px solid black;
+    border-radius: 4px;
+    box-shadow: rgba(18, 18, 20, 0.1) 0px 0px 1px,
+        rgba(18, 18, 20, 0.2) 0px 2px 4px;
+    text-decoration: none;
 `;
 
 const CardImage = styled.div`
     border-radius: 3px;
-    height: 300px;
-    width: 400px;
+    height: 200px;
+    width: 300px;
     background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Mr8cajK9fFcq7qq2Mye76QHaEo%26pid%3DApi&f=1");
     background-size: cover;
     background-position: center;
@@ -22,11 +24,33 @@ const CardImage = styled.div`
 
 const FeaturesBar = styled.div`
     display: flex;
+    margin: 10px 10px;
 `;
 
 const CardHeader = styled.span`
-    font-size: 1.4em,
-    font-weight: 500
+    font-size: 1.2em;
+    font-weight: 700;
+    margin: 0px 20px;
+    margin-top: 10px;
+`;
+
+const AmenityIcon = styled.i`
+    color: #666;
+    margin-right: 6px;
+`;
+
+const InfoHolder = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10px 10px;
+    border-top: 1px solid #c4c4c4;
+`;
+
+const PriceSpan = styled.span`
+    font-size: 1rem;
+    font-weight: 700;
+    margin: 10px 10px;
 `;
 
 const getAmenitiesList = (boat) => {
@@ -43,61 +67,69 @@ export default function BoatCard({ boat }) {
     useEffect(() => {
         const amenitiesList = getAmenitiesList(boat);
         setAmenities(amenitiesList);
+        console.log(amenities);
     }, []);
 
     return (
         <CardContainer>
             <CardImage />
-            <CardHeader>{boat.summary}</CardHeader>
-            <FeaturesBar>
-                {amenities &&
-                    amenities.map((a, idx) => {
-                        switch (a) {
-                            case "hasTv":
-                                return (
-                                    <i
-                                        key={idx}
-                                        className="fab fa-tv fa-1x"
-                                    ></i>
-                                );
+            <CardHeader>
+                {boat.summary.length <= 20
+                    ? boat.summary
+                    : boat.summary.slice(0, 20) + ".."}
+            </CardHeader>
+            <InfoHolder>
+                <FeaturesBar>
+                    {amenities &&
+                        amenities.map((a, idx) => {
+                            switch (a) {
+                                case "hasTv":
+                                    return (
+                                        <AmenityIcon
+                                            key={idx}
+                                            className="fas fa-tv fa-1x"
+                                        ></AmenityIcon>
+                                    );
 
-                            case "hasKitchen":
-                                return (
-                                    <i
-                                        key={idx}
-                                        className="fab fa-kitchen fa-1x"
-                                    ></i>
-                                );
+                                case "hasKitchen":
+                                    return (
+                                        <AmenityIcon
+                                            key={idx}
+                                            className="fas fa-sink fa-1x"
+                                        ></AmenityIcon>
+                                    );
 
-                            case "hasAirConditioning":
-                                return (
-                                    <i
-                                        key={idx}
-                                        className="fab fa-air fa-1x"
-                                    ></i>
-                                );
+                                case "hasAirConditioning":
+                                    return (
+                                        <AmenityIcon
+                                            key={idx}
+                                            className="fas fa-fan fa-1x"
+                                        ></AmenityIcon>
+                                    );
 
-                            case "hasInternet":
-                                return (
-                                    <i
-                                        key={idx}
-                                        className="fab fa-info fa-1x"
-                                    ></i>
-                                );
+                                case "hasInternet":
+                                    return (
+                                        <AmenityIcon
+                                            key={idx}
+                                            className="fas fa-wifi fa-1x"
+                                        ></AmenityIcon>
+                                    );
 
-                            case "hasHeating":
-                                return (
-                                    <i
-                                        key={idx}
-                                        className="fab fa-fire fa-1x"
-                                    ></i>
-                                );
+                                case "hasHeating":
+                                    return (
+                                        <AmenityIcon
+                                            key={idx}
+                                            className="fas fa-fire fa-1x"
+                                        ></AmenityIcon>
+                                    );
 
-                            default:
-                                return null;
-                        }
-                    })}
-            </FeaturesBar>
+                                default:
+                                    return null;
+                            }
+                        })}
+                </FeaturesBar>
+                <PriceSpan>${boat.price}/day</PriceSpan>
+            </InfoHolder>
         </CardContainer>
     );
 }
